340dbea6a02fad390415b434d31c5425958fc129
Luis Alberto <l.al98@hotmail.com>
2023-08-16 22:08:52 -0600

    correction of imags in design

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,6 @@
 const Pagination = ({pages, setCurrentPage, currentPage}) => {
   return (
-    <ul className=" grid grid-flow-col gap-5 justify-center p-10">
+    <ul className=" grid grid-flow-col gap-5 justify-center p-10 text-green-500" >
     {
     pages.map((page) => <button><li className={`${currentPage === page && " text-red-500 h-10 flex items-center justify-center aspect-square bg-green-500/30 border-2 border-green-800"}`} onClick={() => setCurrentPage(page)} key={page}>{page}</li></button>
     
--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -19,16 +19,16 @@ const ResidentCard = ({residentUrl}) => {
   
 
   return (
-    <article className="border-solid border-2 border-green-500 overflow-hidden h-[640px] w-[400px] ">
+    <article className="border-solid border-2 border-green-500 overflow-hidden h-[640px] w-[400px]  transition duration-300 ease-in-out hover:scale-110">
         <header className = " border-solid border-2 border-green-500 " >
-            <img className="object-cover object-center overflow-hidden w-full " src={residentInfo?.image} alt="" />
+            <img className="object-cover object-center overflow-hidden w-full  " src={residentInfo?.image} alt="" />
             <div className=" items-center gap-1 p-2  flex justify-center">
                 <div className={`h-[10px] aspect-square rounded-full flex items-center  ${residentStatus[residentInfo?.status]}`}></div>
                 {residentInfo?.status}
             </div>
         </header>
 
-        <section className="p-2 py-8">
+        <section className="p-2 py-8 h-full bg-gradient-to-r from-green-500 via-black to-green-500 opacity-50 transition duration-300 ease-in-out hover:opacity-90">
             <h3 className="font-bold text-[26px] text-center">{residentInfo?.name}</h3>
             <ol>
                 <li className="font-medium text-[16px] text-gray-400 font-fira flex justify-around">Species: <span className=" font-semibold text-[16px] text-white font-fira ">{residentInfo?.species}</span></li>
